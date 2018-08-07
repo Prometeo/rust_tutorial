dcc30f3c1d611fe8d35ea4aa2e3798f42ca9e098
Xairi <xairi.valdivia@gmail.com>
2018-08-06 23:01:33 -0500

    arrays

---
+++ projects/arrays/src/main.rs
@@ -0,0 +1,23 @@
+// Elements in and array must have the same type
+// and they hav a fixed length
+
+fn main() {
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let first = months[0];
+    let second = months[1];
+    println!("First month: {}", first);
+    println!("Second month: {}", second);
+}
