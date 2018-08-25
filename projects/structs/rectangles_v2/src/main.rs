6716039071e20846cc35f12469b7e6b02829234c
Xairi <xairi.valdivia@gmail.com>
2018-08-24 23:50:28 -0500

    rectangle v2

---
+++ projects/structs/rectangles_v2/src/main.rs
@@ -0,0 +1,10 @@
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle { length: 50, width: 30}    ;
+    println!("rect1 is {:#?}", rect1);
+}
