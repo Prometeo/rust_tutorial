ecd8a8202c690441099411dc7bb9cdab0be130be
Xairi <xairi.valdivia@gmail.com>
2018-12-23 22:50:44 -0500

    cExtra conditionals

---
+++ projects/pattern_matching/extra_conditional/src/main.rs
@@ -0,0 +1,33 @@
+#![allow(unused_variables)]
+fn main() {
+    let num = Some(4);
+
+    match num {
+        Some(x) if x < 5 => println!("less than five: {}", x),
+        Some(x) => println!("{}", x),
+        None => (),
+    }
+}
+
+fn main() {
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {:?}", n),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
+}
+
+fn main() {
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
+}
